438de528a9ddf5713630b9ba078e4245e202ffa6
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2024-02-02 18:59:42 +0100

    Initial commit

---
+++ src/client/java/com/slide_am/HaramCraftClient.java
@@ -0,0 +1,10 @@
+package com.slide_am;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HaramCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
