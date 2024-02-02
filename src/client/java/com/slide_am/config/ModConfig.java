598daf02e94cb0e3ae1a065c98fcf38d68ae22f9
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2024-02-02 21:10:58 +0100

    Add Cloth Config and Mod Menu integration

---
+++ src/client/java/com/slide_am/config/ModConfig.java
@@ -0,0 +1,19 @@
+package com.slide_am.config;
+
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+
+@Config(name = "haramcraft")
+public class ModConfig implements ConfigData {
+
+    @ConfigEntry.Gui.Tooltip
+    public boolean enableExplosion = true;
+
+    @ConfigEntry.Gui.Tooltip
+    public boolean enableTeleportation = true;
+
+    @ConfigEntry.Gui.Tooltip
+    public float explosionPower = 5;
+}
--- src/main/java/com/slide_am/event/PorkConsumeEvent.java
+++ src/main/java/com/slide_am/event/PorkConsumeEvent.java
@@ -1,5 +1,9 @@
 package com.slide_am.event;
 
+
+import com.slide_am.config.ModConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -11,17 +15,27 @@ import static net.minecraft.world.World.ExplosionSourceType.TNT;
 import static net.minecraft.world.World.NETHER;
 
 public class PorkConsumeEvent {
+
     public static void init(){
+
+        AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
+        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+
         UseItemCallback.EVENT.register((player, world, hand) -> {
             if(!world.isClient){
                 if(world.getRegistryKey() != NETHER){
                     if(player.getStackInHand(hand).getItem() == Items.PORKCHOP ||
                             player.getStackInHand(hand).getItem() == Items.COOKED_PORKCHOP
                     ){
-                        player.sendMessage(Text.of("HARAM"));
-                        world.createExplosion(player, player.getX(), player.getY(), player.getZ(), 32, TNT);
-                        ServerWorld nether = player.getServer().getWorld(NETHER);
-                        player.teleport(nether, player.getX(), player.getY(), player.getZ(), null, player.getYaw(), player.getPitch());
+                        player.sendMessage(Text.of(String.valueOf(config.explosionPower)));
+                        if(config.enableExplosion){
+                            world.createExplosion(player, player.getX(), player.getY(), player.getZ(), config.explosionPower, TNT);
+                        }
+
+                        if(config.enableTeleportation){
+                            ServerWorld nether = player.getServer().getWorld(NETHER);
+                            player.teleport(nether, player.getX(), player.getY(), player.getZ(), null, player.getYaw(), player.getPitch());
+                        }
                     }
                 }
             }
